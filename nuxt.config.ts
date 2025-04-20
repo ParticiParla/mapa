01ef79750e711113ddf55c153837116615b0f2ae
Eneko Rodríguez <eneko.rodriguez@rrverse.com>
2025-04-20 15:19:06 +0200

    fix: build

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,24 +1,23 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-				compatibilityDate: '2024-04-03',
-				ssr: false,
-				devtools: { enabled: false },
-				modules: [
-				 '@unocss/nuxt',
-				 "@nuxtjs/leaflet",
-				 "@nuxt/image",
-				 "@nuxt/fonts",
-				 '@nuxt/ui',
-				 '@nuxthub/core',
-				],
-				css: ['~/assets/css/main.css'],
-				runtimeConfig: {
-								directusUrl: process.env.DIRECTUS_URL, // Solo disponible en el servidor
-								directusToken: process.env.DIRECTUS_STATIC_TOKEN, // Solo disponible en el servidor
+	compatibilityDate: '2024-04-03',
+	devtools: { enabled: false },
+	modules: [
+		'@unocss/nuxt',
+		"@nuxtjs/leaflet",
+		"@nuxt/image",
+		"@nuxt/fonts",
+		'@nuxt/ui',
+		'@nuxthub/core',
+	],
+	css: ['~/assets/css/main.css'],
+	runtimeConfig: {
+		directusUrl: process.env.DIRECTUS_URL, // Solo disponible en el servidor
+		directusToken: process.env.DIRECTUS_STATIC_TOKEN, // Solo disponible en el servidor
 
 
-								// public: {
-								// 	directusUrl: process.env.DIRECTUS_URL
-								// }
-				},
+		// public: {
+		// 	directusUrl: process.env.DIRECTUS_URL
+		// }
+	},
 })
